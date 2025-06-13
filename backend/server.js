c0ce551707390575398452ce938459e5adef6a34
Parthmh361 <choudhariparth3612@gmail.com>
2025-06-13 17:05:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -55,5 +55,6 @@ app.use('/api/youtube', require('./routes/youtube'));
 // ✅ Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
+  alert("Hello");
   console.log(`Server running on http://localhost:${PORT}`);
 });
