f0223a975cf2b9920392743dbbe9eeee801cc97d
Parthmh361 <choudhariparth3612@gmail.com>
2025-06-13 17:52:08 +0530

    Update AuthRoutes.js

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -20,7 +20,7 @@ router.get('/facebook', async (req, res) => {
   const { user_id } = req.query; // Pass user_id as query param
   try {
     const { clientId } = await getFacebookCredentials(user_id);
-     const REDIRECT_URI = `https://socialsuit-backend-h9md.onrender.com/auth/facebook/callback?user_id=${encodeURIComponent(user_id)`;
+     const REDIRECT_URI = `https://socialsuit-backend-h9md.onrender.com/auth/facebook/callback?user_id=${encodeURIComponent(user_id)}`;
     const authURL = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${clientId}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=read_insights,pages_show_list,pages_read_engagement,pages_manage_posts,pages_read_user_content,pages_manage_metadata,pages_show_list,&response_type=code`;
     res.redirect(authURL);
   } catch (err) {
